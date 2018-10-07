6b582dcd1122f229851ea85952d3448baac7da16
crazyayane <crazyayane07@gmail.com>
2018-10-07 10:07:54 +0200

    drag

--- Assets/Scripts/Static/Constans.cs
+++ Assets/Scripts/Static/Constans.cs
@@ -3,6 +3,8 @@ namespace Rehab
 {
     public class Constans
     {
+        public const string DRAGGABLE_TAG = "UIDraggable";
+
         public static string AdminPassword = "aaa";
 
         public static string AuthorizeError = "Podano zle haslo!";
